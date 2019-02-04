56b6744b12e93cbb53d4b7dfe3e751c5ac5f2bd4
Kane Clover <kane@assure24.com>
2019-02-04 16:53:55 +0000

    Config examples

---
+++ config.example.js
@@ -0,0 +1,4 @@
+module.exports = {
+  geocodingAPIKey: 'GOOGLE_MAPS_API_KEY_HERE',
+  userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
+}
