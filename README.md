c2d873c9231d307a52f0c43876bf8aac89ec2363
Kane Clover <kane@assure24.com>
2019-05-05 10:19:31 +0100

    Add the rest of the MCR councils minus tameside, who don't use idox.

--- README.md
+++ README.md
@@ -10,3 +10,10 @@ Hence this.
 Create `config.js` using the format in `config.example.js` and you're good to go.
 
 Tested on Node v10.4.1.
+
+Deploy with [PM2](https://pm2.io/), configured in `ecosystem.config.js`. `pm2 start` sets a cron job up to run things weekly.
+
+Logs stored in `log/` with winston. JSON format. Best viewed with something like [json-log-viewer](https://www.npmjs.com/package/json-log-viewer) I think. `jv logs/combined.log`
+
+Also posts warn level log messages to Slack channel. See hook in config.
+ 
--- index.js
+++ index.js
@@ -20,12 +20,18 @@ async function doScrape() {
 
     // Think all these use "idox": http://www.idoxgroup.com/
     const idoxSites = [
+      // Greater manchester councils
+      // https://en.wikipedia.org/wiki/Greater_Manchester
       "https://pa.manchester.gov.uk/online-applications",
       "https://publicaccess.trafford.gov.uk/online-applications",
       "https://www.planningpa.bolton.gov.uk/online-applications-17",
       "https://planning.bury.gov.uk/online-applications",
       "http://planningpa.oldham.gov.uk/online-applications",
-      "http://publicaccess.rochdale.gov.uk/online-applications"
+      "http://publicaccess.rochdale.gov.uk/online-applications",
+      "http://publicaccess.salford.gov.uk/publicaccess",
+      "http://planning.stockport.gov.uk/PlanningData-live",
+      "https://planning.wigan.gov.uk/online-applications"
+      // tameside is not idox: https://www.tameside.gov.uk/planning/applications
     ];
 
     for (let i = 0; i < idoxSites.length; i++) {
